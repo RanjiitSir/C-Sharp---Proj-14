2a619af3aeb311d65b9137baa26166c2b4cfc556
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-04-09 19:41:46 +0530

    Add files via upload

---
+++ Inventory App Project April 2026/ClassFiles/Input_Validator_CID.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CID_Billing_App.ClassFiles
+{
+    public static class Input_Validator_CID
+    {
+        public static void AllowOnlyNumeric(KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar);
+        }
+
+        public static void AllowNumericAmount(KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != (char)Keys.Back;
+        }
+
+        public static void AllowOnlyText(KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && e.KeyChar != ' ';
+        }
+
+        public static void AllowAlphaNumeric(KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != ' ';
+        }
+    }
+}
---
+++ Inventory App Project April 2026/ClassFiles/Shared_IDs_CID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CID_Billing_App.ClassFiles
+{
+    public static class Shared_IDs_CID
+    {
+        public static int Cust_Bill_ID { get; set; } // remove from ere. Send as a Parameter
+    }
+}
